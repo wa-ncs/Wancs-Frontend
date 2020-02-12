3c7e50d9be95447f10069cbc5f8e0d30a0988d57
Bahng Ji Hoon <wpxm2003@naver.com>
2020-02-12 23:24:01 +0900

    필요없는 코드 제거

--- wancs/src/Components/Board.js
+++ wancs/src/Components/Board.js
@@ -2,17 +2,16 @@ import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 
-const Board = ({boardNo, title, views, id, content}) => {
-    return (
-        <TableRow>
-            <TableCell>{boardNo}</TableCell>
-            <TableCell>{title}</TableCell>
-            <TableCell>{id}</TableCell>
-            <TableCell>{views}</TableCell>
-            <TableCell>{content}</TableCell>
-        </TableRow>
-    )
-
-}
+const Board = ({ boardNo, title, views, id, content }) => {
+  return (
+    <TableRow>
+      <TableCell>{boardNo}</TableCell>
+      <TableCell>{title}</TableCell>
+      <TableCell>{id}</TableCell>
+      <TableCell>{views}</TableCell>
+      <TableCell>{content}</TableCell>
+    </TableRow>
+  );
+};
 
 export default Board;
--- wancs/src/Components/BoardList.js
+++ wancs/src/Components/BoardList.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,51 +7,43 @@ import TableCell from '@material-ui/core/TableCell';
 import Board from './Board';
 
 const BoardList = () => {
-    const [boardList, setBoardList] = useState("");
+  const [boardList, setBoardList] = useState('');
 
-    // useEffect로 하기전 코드
-    // const onBoardList = async () => {
-    //     const response = await fetch('/v1/board');
-    //     const body = await response.json();
-    //     console.log(body);
-    //     setBoardList(body);
-    // }
+  useEffect(() => {
+    async function getList() {
+      const response = await fetch('/v1/board');
+      const body = await response.json();
+      console.log(body);
+      setBoardList(body);
+    }
+    getList();
+  }, []); //끝에 빈 배열을 넣어주면 초기 렌더링때만 실행하고 update 땐 동작안함
 
-    useEffect(()=>{
-        async function getList(){
-            const response = await fetch('/v1/board');
-            const body = await response.json();
-            console.log(body);
-            setBoardList(body);
-        }
-        getList();
-        
-    }, []) //끝에 빈 배열을 넣어주면 초기 렌더링때만 실행하고 update 땐 동작안함
+  return (
+    <div>
+      {boardList ? (
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>제목</TableCell>
+              <TableCell>작성자</TableCell>
+              <TableCell>조회수</TableCell>
+              <TableCell>내용</TableCell>
+            </TableRow>
+          </TableHead>
 
-    return (
-        <div>
-            {/* <button onClick={onBoardList}>게시판 조회</button> */}
-            {boardList ? 
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>번호</TableCell>
-                        <TableCell>제목</TableCell>
-                        <TableCell>작성자</TableCell>
-                        <TableCell>조회수</TableCell>
-                        <TableCell>내용</TableCell>
-                    </TableRow>
-                </TableHead>
-                
-                <TableBody>
-                    {boardList.map(b => {
-                        return (<Board key={b.boardNo} {...b} />)
-                    })}
-                </TableBody>
-            </Table>
-:""}
-        </div>
-    );
+          <TableBody>
+            {boardList.map(b => {
+              return <Board key={b.boardNo} {...b} />;
+            })}
+          </TableBody>
+        </Table>
+      ) : (
+        'Loading...'
+      )}
+    </div>
+  );
 };
 
 export default BoardList;
